016a905a27189d0901d1f6bafbb7b1cdf6d57b93
nenahui <kanatraccoon@gmail.com>
2024-08-24 12:55:40 +0600

    Init shadcn ui library

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import { ThemeProvider } from '@/components/ThemeProvider/ThemeProvider';
 
 export const App: React.FC = () => {
-	return <>App</>;
+  return <ThemeProvider defaultTheme={'dark'}>App</ThemeProvider>;
 };
